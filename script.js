dc98f6aa0b0cd23c133b80a1f956b15d3fd0374a
MrWcW <149218298+MrWcW@users.noreply.github.com>
2025-02-28 15:27:22 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
-// MOBILE GESTURES
-var myElement = document.getElementById('mobilewrap');
+// Mobile Gesture controls using Hammer.js
+var myElement = document.getElementById('mobilewrap'); // Your game container element
 var hammertime = new Hammer(myElement);
+
+// Enable swipe gestures in all directions
 hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
 
+// Swipe left (move left)
 hammertime.on('swipeleft', function(ev) {
-  moveDirection(37);
+  moveDirection(37); // Equivalent to pressing the left arrow key
 });
 
+// Swipe right (move right)
 hammertime.on('swiperight', function(ev) {
-  moveDirection(39);
+  moveDirection(39); // Equivalent to pressing the right arrow key
 });
 
+// Swipe up (move up)
 hammertime.on('swipeup', function(ev) {
-  moveDirection(38);
+  moveDirection(38); // Equivalent to pressing the up arrow key
 });
 
+// Swipe down (move down)
 hammertime.on('swipedown', function(ev) {
-  moveDirection(40);
+  moveDirection(40); // Equivalent to pressing the down arrow key
 });
 
 /* ---------------------------------------------------------------------- */
 
+// Game logic (from your original code)
 var matrix = [[0, 0, 0, 0],
               [0, 0, 0, 0],
               [0, 0, 0, 0],
@@ -29,6 +36,7 @@ var component = new Array();
 var best = 0;
 var score = 0;
 
+// Reset the game
 $(".button").on("click", function(){
   restoreField();
   init();
@@ -36,6 +44,7 @@ $(".button").on("click", function(){
 
 init();
 
+// Function to reset the game state
 function restoreField() {
   score = 0;
   $(".scorefield").text(score);
@@ -49,14 +58,17 @@ function restoreField() {
   $(".over").css("visibility", "hidden").css("opacity", "0");
 }
 
+// Random number generator
 function random(min, max) {
   return Math.floor((Math.random() * max) + min);
 }
 
+// Randomly pick between 2 or 4 for a new tile
 function dueoquattro() {
   return (((Math.random() * 10) > 5) ? 4 : 2);
 }
 
+// Initialize the game
 function init() {
   var i = 0;
   while(i < 2){
@@ -73,28 +85,17 @@ function init() {
   }
 }
 
+// Update the position of tiles on the screen
 function updateTile(trax, tray, x, y) {
-  var tileValue = matrix[x][y]; // Get the value of the tile at matrix[x][y]
+  var tileValue = matrix[x][y]; 
 
-  // Add the appropriate class for the tile based on its value
   $(".tiles").append("<div class='tile tile-" + tileValue + " tile-" + x + "-" + y + "' style='transform: translate(" + trax + "vh, " + tray + "vh);'>" +
     "<div class='tile_content'>" +
     "<span>" + tileValue + "</span>" +
     "</div></div>");
 }
 
-window.addEventListener('keydown', this.direction, false);
-
-function compare(a, b) {
-  if(dx == 1){
-    if (a.x < b.x)
-      return -1;
-    if (a.x > b.x)
-      return 1;
-    return 0;
-  }
-}
-
+// Function to handle movement
 function moveDirection(code) {
   var change = 0;
   switch(code) {
@@ -125,32 +126,7 @@ function moveDirection(code) {
   }
 }
 
-function checkDefeat(){
-  if(component.length == 16) {
-    for(var i = 0; i < component.length; i++) {
-      for(var x = -1; x <= 1; x++) {
-        for(var y = -1; y <= 1; y++) {
-          if(x != y && Math.abs(x) != Math.abs(y)) {
-            if(isMovePossible(component[i].x, component[i].y, x, y, i)) {
-              return false;
-            }
-          }
-        }
-      }
-    }
-
-    return true;
-  }
-}
-
-function won(){
-  $(".won").css("visibility", "visible").css("padding-top", "0px").css("opacity", 1);
-}
-
-function direction(e) {
-  moveDirection(e.keyCode);
-}
-
+// Add a new tile to the board
 function addTile() {
   var i = 0;
   while(i < 1) {
@@ -163,12 +139,12 @@ function addTile() {
       matrix[x][y] = tileValue;
       component.push({x: x, y: y});
 
-      // Use updateTile to add the tile to the grid
       updateTile(12 * x, 12 * y, x, y);
     }
   }
 }
 
+// Move function to check if tiles can move
 function move(dx, dy) {
   var change = 0;  
   for(var i = 0; i < component.length; i++) {
@@ -186,6 +162,7 @@ function move(dx, dy) {
   return change;
 }
 
+// Make the move and update the tile positions
 function makeMove(x, y, dx, dy, i) {
   var newX = x + dx;
   var newY = y + dy;  
@@ -213,14 +190,31 @@ function makeMove(x, y, dx, dy, i) {
   }
 }
 
-function checkTrash() {
-  for(var i = 0; i < component.length; i++) {
-    if(component[i].x == -1 && component[i].y == -1) {
-      component.splice(i, 1);
+// Check for defeat condition
+function checkDefeat(){
+  if(component.length == 16) {
+    for(var i = 0; i < component.length; i++) {
+      for(var x = -1; x <= 1; x++) {
+        for(var y = -1; y <= 1; y++) {
+          if(x != y && Math.abs(x) != Math.abs(y)) {
+            if(isMovePossible(component[i].x, component[i].y, x, y, i)) {
+              return false;
+            }
+          }
+        }
+      }
     }
+
+    return true;
   }
 }
 
+// Function to display when the player wins
+function won(){
+  $(".won").css("visibility", "visible").css("padding-top", "0px").css("opacity", 1);
+}
+
+// Function to check if a move is possible
 function isMovePossible(x, y, dx, dy, i) {
   var newX = x + dx;
   var newY = y + dy;
@@ -237,3 +231,5 @@ function isMovePossible(x, y, dx, dy, i) {
     return false;
   }
 }
+
+init();
